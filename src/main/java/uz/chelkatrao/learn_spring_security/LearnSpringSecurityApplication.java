08f8f331d37e889951f9da69c229c782c6bf6426
chelk <chelkatrao@gmail.com>
2024-12-22 22:59:17 +0500

    feat: add v4 get User info method

--- src/main/java/uz/chelkatrao/learn_spring_security/LearnSpringSecurityApplication.java
+++ src/main/java/uz/chelkatrao/learn_spring_security/LearnSpringSecurityApplication.java
@@ -3,9 +3,16 @@ package uz.chelkatrao.learn_spring_security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 public class LearnSpringSecurityApplication {
@@ -26,12 +33,19 @@ public class LearnSpringSecurityApplication {
 
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
-//        return RouterFunctions.route()
-//                .GET("/api/v4/greetings", request -> {
-//
-//                })
-//                .build();
-        return null;
+        return RouterFunctions.route()
+                .GET("/api/v4/greetings", request -> {
+                    Optional<Principal> principal = request.principal();
+                    UserDetails userDetails = principal
+                            .map(Authentication.class::cast)
+                            .map(Authentication::getPrincipal)
+                            .map(UserDetails.class::cast)
+                            .orElseThrow();
+                    return ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(Map.of("greeting", "Hello, %s! V4".formatted(userDetails.getUsername())));
+                })
+                .build();
     }
 
 }
