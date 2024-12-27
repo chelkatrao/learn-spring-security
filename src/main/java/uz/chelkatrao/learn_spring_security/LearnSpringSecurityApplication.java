9f31e359af507e4bf56885a44c7e2e70504026b2
Ashraf <chelkatrao@gmail.com>
2024-12-27 10:31:55 +0500

    feat: simple basic auth

--- src/main/java/uz/chelkatrao/learn_spring_security/LearnSpringSecurityApplication.java
+++ src/main/java/uz/chelkatrao/learn_spring_security/LearnSpringSecurityApplication.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
@@ -27,17 +26,9 @@ public class LearnSpringSecurityApplication {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        BasicAuthenticationEntryPoint authenticationEntryPoint = new BasicAuthenticationEntryPoint();
-        authenticationEntryPoint.setRealmName("Realm");
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .exceptionHandling(eh-> eh.authenticationEntryPoint(authenticationEntryPoint))
-                .httpBasic(httpBasic -> {
-                    httpBasic.authenticationEntryPoint((request, response, authException) -> {
-                        authException.printStackTrace();
-                        authenticationEntryPoint.commence(request, response, authException);
-                    });
-                })
+                .authorizeHttpRequests(e -> e.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
